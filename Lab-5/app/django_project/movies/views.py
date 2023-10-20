a22df7f07d098ee849dd0413427e67b8412cefae
Cristian-Araya-Cubillos <80790384+Cristian-Araya-Cubillos@users.noreply.github.com>
2023-10-20 09:20:14 -0300

    Añadiendo Lab 5

--- Lab-5/app/django_project/movies/views.py
+++ Lab-5/app/django_project/movies/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-import requests
+from .services import generate_request
 
 def index(request):
-    
-
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'generos':generate_request()}) #Se manda a establecer la conexion con la pagina,
+                                                                        #si esta retorna un codigo 200 (OK), significa que se establecio
+                                                                        #la conexion correctamente, luego mandamos los generos
+                                                                        #como parametro a index.html
